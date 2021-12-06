fab5d68d16b5a958507a61876035f57a47d2dbdb
dejan <dejan.prv@gmail.com>
2021-12-06 18:56:14 +0100

    add 2_create_big_json - not finished

---
+++ ETL/2_create_big_json.py
@@ -0,0 +1,5 @@
+from pathlib import Path 
+from pprint import pprint
+
+p = Path("./ETL/data").resolve()
+
--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,5 @@
 env\Scripts\activate
+
+# ETL
+python ETL\1_run_scrappers.py
+python ETL\2_create_big_json.py
