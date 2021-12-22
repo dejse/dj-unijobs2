cd1ad454b509857cf0a6ca79537b8dc3fdf70816
dejan <dejan.prv@gmail.com>
2021-12-22 19:48:06 +0100

    add language to tests

--- Backend/jobs/test.py
+++ Backend/jobs/test.py
@@ -1,5 +1,7 @@
 from django.test import TestCase 
+from django.test.client import Client
 from django.urls import reverse
+from django.utils.translation import activate
 
 class TestSite(TestCase):
   def test_index(self):
@@ -7,17 +9,21 @@ class TestSite(TestCase):
     self.assertEqual(response.status_code, 200)
 
   def test_index_de(self):
+    activate("de")
     response = self.client.get("/de")
     self.assertEqual(response.status_code, 301)
 
   def test_index_en(self):
+    activate("en")
     response = self.client.get("/en")
     self.assertEqual(response.status_code, 301)
 
   def test_search_form_empty(self):
+    activate("de")
     response = self.client.post("/suche", { "job": "", "uni": "" })
     self.assertEqual(response.status_code, 302)
 
-  def test_search_formy(self):
+  def test_search_form_input(self):
+    activate("de")
     response = self.client.post("/suche", { "job": "prof", "uni": "Wien" })
-    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.status_code, 302)
